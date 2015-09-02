dc3b645888787f959ee5a2affbe2f55b75268302
Erick Ribeiro <erick.ribeiro.17@hotmail.com>
2015-09-01 22:44:29 -0400

    Criando o Leitor de QR Code

--- app/src/main/java/com/eribeiro/projectinterstellar/MainActivity.java
+++ app/src/main/java/com/eribeiro/projectinterstellar/MainActivity.java
@@ -1,17 +1,67 @@
 package com.eribeiro.projectinterstellar;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView myTextView;
+    private QRCodeReaderView mydecoderview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        myTextView = (TextView) findViewById(R.id.exampleTextView);
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        myTextView.setText(text);
+
+        Log.d("RESULTADO", text);
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+        Log.d("RESULTADO", myTextView.getText().toString());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
+        Log.d("RESULTADO", myTextView.getText().toString());
     }
 
 
